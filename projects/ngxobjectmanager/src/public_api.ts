e56baab7fe53a6fa29ccc1ad59ddf9aad92749f3
David Barnes <david.barnes@heretik.com>
2018-11-07 16:10:29 -0600

    maybe fixed barrel problem

--- projects/ngxobjectmanager/src/public_api.ts
+++ projects/ngxobjectmanager/src/public_api.ts
@@ -2,9 +2,5 @@
  * Public API Surface of ngxobjectmanager
  */
 
-export * from './lib/object-manager/ngxobjectmanager.service';
-export * from './lib/choice-query-manager';
-export * from './lib/list-view-manager';
 export { toCamel } from './lib/utils';
 export * from './lib/ngxobjectmanager.module';
-export * from './lib/object-manager';
